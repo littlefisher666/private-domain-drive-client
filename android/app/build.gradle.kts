31a31f48cbf21fb8911df3c5f2af65db92b5358e
jinyanan <jinyanan@hdzk.cn>
2026-09-11 16:44:52 +0800

    build: 统一客户端应用标识符

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,7 +14,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.privatedomain.drive.private_domain_drive_client"
+    namespace = "com.github.littlefisher666.private_domain_drive_client"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -25,7 +25,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.privatedomain.drive.private_domain_drive_client"
+        applicationId = "com.github.littlefisher666.private_domain_drive_client"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/github/littlefisher666/private_domain_drive_client/MainActivity.kt
+++ android/app/src/main/kotlin/com/github/littlefisher666/private_domain_drive_client/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.privatedomain.drive.private_domain_drive_client
+package com.github.littlefisher666.private_domain_drive_client
 
 import android.content.Intent
 import android.content.pm.PackageManager
